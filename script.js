eaf83ee8be669dec02d8b668d163c10e81060284
Bob Klasen <robfklasen@gmail.com>
2022-10-23 16:51:47 -0500

    Finalized general styles (fonts, small adjustments). Mobile responsivness added. Styled for screens under 850px and over 850px

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const mobileNav = document.getElementById('mobileNav');
+const mobileLinks = document.querySelector('.topnav');
+
+function displayMobileLinks() {
+  console.log('teste');
+  if (mobileLinks.style.display === 'block') {
+    mobileLinks.style.display = 'none';
+  } else {
+    mobileLinks.style.display = 'block';
+  }
+}
+
+mobileNav.addEventListener('click', displayMobileLinks);
