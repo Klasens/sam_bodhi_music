3cab94b6dd911214ff5d90343b28e5f8bdedd74d
Bob Klasen <robfklasen@gmail.com>
2022-12-20 09:32:15 -0600

    Updated contact and video page styling.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const mobileNav = document.getElementById('mobileNav');
 const mobileLinks = document.querySelector('.topnav');
 const scrollArrow = document.querySelector('#landingArrow');
 const scrollToLanding = document.querySelector('#landingBio');
+const contactImages = document.querySelector('.mainContent-contact');
+const form = document.querySelector('.contactForm');
 
 function displayMobileLinks() {
   if (mobileLinks.style.display === 'block') {
@@ -13,6 +15,10 @@ function displayMobileLinks() {
   }
 }
 
+window.addEventListener('DOMContentLoaded', function () {
+  console.log('test');
+  form.classList.add('fade-in');
+});
 mobileNav.addEventListener('click', displayMobileLinks);
 scrollArrow.addEventListener('click', function (e) {
   e.preventDefault();
