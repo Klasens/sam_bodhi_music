2e284ce98759f35f2cfb3d08d76d1a123b3d18a9
Klasens <57231452+Klasens@users.noreply.github.com>
2023-01-22 19:59:02 -0600

    Finshed Modals, changed images, added buttons for modals

--- js/modals.js
+++ js/modals.js
@@ -1,9 +1,9 @@
 //outer containers
-const outerModalTwo = document.querySelector('.outerModalTwo');
+const outerModal = document.querySelector('.modal__songs--outer');
 
 //btns
 const jazzBtn = document.querySelector('#jazz');
-const jazzModal = document.querySelector('.modal__songs--outer');
+const jazzModal = document.querySelector('.modal__songs--jazz');
 const bluesBtn = document.querySelector('#blues');
 const bluesModal = document.querySelector('#bluesModal');
 const americanaBtn = document.querySelector('#americana');
@@ -13,7 +13,7 @@ const originalModal = document.querySelector('#originalModal');
 
 //modal close
 const modalClose = document.querySelectorAll('.modal__close');
-const modalCloseTwo = document.querySelectorAll('.modal-close--shows');
+const modalCloseTwo = document.querySelectorAll('.modal__close--songs');
 
 jazzModal.addEventListener('click', function (e) {
   e.preventDefault();
@@ -42,6 +42,8 @@ jazzBtn.addEventListener('click', function (e) {
   e.stopImmediatePropagation();
   jazzModal.classList.add('fadeIn');
   jazzModal.classList.remove('pointerEvents-none');
+  outerModal.classList.add('fadeIn');
+  outerModal.classList.remove('pointerEvents-none');
 });
 bluesBtn.addEventListener('click', function (e) {
   e.preventDefault();
@@ -49,6 +51,8 @@ bluesBtn.addEventListener('click', function (e) {
   e.stopImmediatePropagation();
   bluesModal.classList.add('fadeIn');
   bluesModal.classList.remove('pointerEvents-none');
+  outerModal.classList.add('fadeIn');
+  outerModal.classList.remove('pointerEvents-none');
 });
 americanaBtn.addEventListener('click', function (e) {
   e.preventDefault();
@@ -56,6 +60,8 @@ americanaBtn.addEventListener('click', function (e) {
   e.stopImmediatePropagation();
   americanaModal.classList.add('fadeIn');
   americanaModal.classList.remove('pointerEvents-none');
+  outerModal.classList.add('fadeIn');
+  outerModal.classList.remove('pointerEvents-none');
 });
 originalBtn.addEventListener('click', function (e) {
   e.preventDefault();
@@ -63,6 +69,8 @@ originalBtn.addEventListener('click', function (e) {
   e.stopImmediatePropagation();
   originalModal.classList.add('fadeIn');
   originalModal.classList.remove('pointerEvents-none');
+  outerModal.classList.add('fadeIn');
+  outerModal.classList.remove('pointerEvents-none');
 });
 
 for (let i = 0; i < modalClose.length; i++) {
@@ -88,6 +96,8 @@ for (let i = 0; i < modalCloseTwo.length; i++) {
     americanaModal.classList.add('pointerEvents-none');
     originalModal.classList.remove('fadeIn');
     originalModal.classList.add('pointerEvents-none');
+    outerModal.classList.remove('fadeIn');
+    outerModal.classList.add('pointerEvents-none');
   });
 }
 
@@ -112,5 +122,20 @@ document.addEventListener('keydown', (event) => {
     americanaModal.classList.add('pointerEvents-none');
     originalModal.classList.remove('fadeIn');
     originalModal.classList.add('pointerEvents-none');
+    outerModal.classList.remove('fadeIn');
+    outerModal.classList.add('pointerEvents-none');
   }
+});
+
+outerModal.addEventListener('click', function () {
+  jazzModal.classList.remove('fadeIn');
+  jazzModal.classList.add('pointerEvents-none');
+  bluesModal.classList.remove('fadeIn');
+  bluesModal.classList.add('pointerEvents-none');
+  americanaModal.classList.remove('fadeIn');
+  americanaModal.classList.add('pointerEvents-none');
+  originalModal.classList.remove('fadeIn');
+  originalModal.classList.add('pointerEvents-none');
+  outerModal.classList.remove('fadeIn');
+  outerModal.classList.add('pointerEvents-none');
 });
